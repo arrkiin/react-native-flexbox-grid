1aab201290c009d8d82551b91b57b3b2600da30f
arrkiin <arrkiin@gmail.com>
2018-04-08 20:37:17 +0200

    Make column a class component with state-change to update during resize

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,11 +1,27 @@
-import React, {Component} from 'react';
-import PropTypes from "prop-types";
-import {getScreenSize} from '../lib/ScreenSize';
-import {isHidden, getComponentWidth, getComponentOffset} from '../lib/helpers';
-import {View} from 'react-native';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getScreenSize } from '../lib/ScreenSize';
+import {
+  isHidden,
+  getComponentWidth,
+  getComponentOffset,
+} from '../lib/helpers';
+import { View } from 'react-native';
 
-const Column = (props) => {
-    const screenSize = getScreenSize()
+class Column extends Component {
+  state = {
+    width: 0,
+  };
+  componentDidMount() {
+    this.updateSize();
+  }
+  updateSize = () => {
+    const { gridProps } = this.getGridProps(this.props);
+    this.setState({
+      width: getComponentWidth(getScreenSize(), gridProps),
+    });
+  };
+  getGridProps = props => {
     const {
       sm,
       smOffset,
@@ -30,26 +46,36 @@ const Column = (props) => {
       lg,
       lgOffset,
       lgHidden,
-      rowSize
+      rowSize,
     };
 
-    if(isHidden(screenSize, gridProps)){
+    return { gridProps, rest };
+  };
+  render() {
+    const { gridProps, rest } = this.getGridProps(this.props);
+
+    if (isHidden(getScreenSize(), gridProps)) {
       return null;
     } else {
       return (
         <View
-        {...rest}
-        style={[
-          props.style, {
-            width: getComponentWidth(screenSize, gridProps),
-            flexDirection: 'column',
-            marginLeft: getComponentOffset(screenSize, gridProps)
-          }]}>
+          onLayout={this.updateSize.bind(this)}
+          {...rest}
+          style={[
+            this.props.style,
+            {
+              width: getComponentWidth(getScreenSize(), gridProps),
+              flexDirection: 'column',
+              marginLeft: getComponentOffset(getScreenSize(), gridProps),
+            },
+          ]}
+        >
           {rest.children}
         </View>
       );
     }
-};
+  }
+}
 
 Column.propTypes = {
   sm: PropTypes.number,
